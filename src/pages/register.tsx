8d6b7ee93129dbbefb68608ac75b2bcdf935528a
Jonathan Morris <jonathan.morris@slalom.com>
2021-11-24 15:45:51 -0700

    working on Input component error

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,10 +1,49 @@
-import React from 'react';
-import { Formik } from 'formik';
+import React from "react";
+import { Form, Formik } from "formik";
+import { Wrapper } from "../components/Wrapper";
+import { InputField } from "../components/InputField";
+import { Box } from "@chakra-ui/layout";
+import { Button } from "@chakra-ui/button";
 
 interface RegisterProps {}
 
 export const Register: React.FC<RegisterProps> = ({}) => {
-    return <div></div>;
+    return (
+        <Wrapper variant="small">
+            <Formik
+                initialValues={{ username: "", password: "" }}
+                onSubmit={values => {
+                    console.log(values);
+                }}
+            >
+                {({ values, handleChange, isSubmitting }) => (
+                    <Form>
+                        <InputField
+                            name="username"
+                            placeholder="username"
+                            label="username"
+                        ></InputField>
+                        <Box mt={4}>
+                            <InputField
+                                name="password"
+                                placeholder="password"
+                                label="password"
+                                type="password"
+                            ></InputField>
+                        </Box>
+                        <Button
+                            mt={4}
+                            type="submit"
+                            colorScheme="teal"
+                            isLoading={isSubmitting}
+                        >
+                            Register
+                        </Button>
+                    </Form>
+                )}
+            </Formik>
+        </Wrapper>
+    );
 };
 
 export default Register;
