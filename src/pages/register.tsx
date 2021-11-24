0fa1d2020cd382a93f97c8f7c9d412afa0b2cad0
Jonathan Morris <jonathan.morris@slalom.com>
2021-11-24 14:22:59 -0700

    initial commit

---
+++ src/pages/register.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Formik } from 'formik';
+
+interface RegisterProps {}
+
+export const Register: React.FC<RegisterProps> = ({}) => {
+    return <div></div>;
+};
+
+export default Register;
